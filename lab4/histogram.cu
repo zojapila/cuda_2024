e81a3f67e0f11515de41327ef973ddaef82323b8
zojapila@gmail.com <zojapila@gmail.com>
2024-10-27 17:52:09 +0100

    dziala hist1

--- lab4/histogram.cu
+++ lab4/histogram.cu
@@ -2,11 +2,22 @@
 #include <stdio.h>
 
 #define N_LETTERS 26
+#define WARPS_SIZE 32
 
 //@@ INSERT CODE HERE
 // Histogram - basic parallel implementation
 __global__ void histogram_1(unsigned char *buffer, long size, unsigned int *histogram, unsigned int nBins)
 {
+	int binWidth = ceil(26.0/nBins);
+	for (int i = 0; i < size; i++)
+	{
+		int alphabetPosition = buffer[i] - 'a';
+		if (alphabetPosition >= 0 && alphabetPosition < 26) 
+		{
+			histogram[alphabetPosition / binWidth]++;
+		}
+	}
+
 }
 
 //@@ INSERT CODE HERE
@@ -59,10 +70,36 @@ int main(int argc, char **argv)
 
 	///////////////////////////////////////////////////////
 	//@@ INSERT CODE HERE
+	int dimBlock = 256;
+	int dimGrid = (size + dimBlock - 1) / dimBlock;
+	unsigned char *d_buffer;
+	unsigned int *d_histogram, *h_histogram;
+	h_histogram = (unsigned int *)malloc(nBins * sizeof(unsigned int));
+	cudaMemset(h_histogram, 0, nBins * sizeof(unsigned int));
+	cudaMalloc((void **)&d_buffer, size * sizeof(unsigned char));
+	cudaMalloc((void **)&d_histogram, nBins * sizeof(unsigned int));
+	cudaMemcpy(d_buffer, h_buffer, size * sizeof(unsigned char), cudaMemcpyHostToDevice);
+	cudaMemset(d_histogram, 0, nBins * sizeof(unsigned int));
+	histogram_1<<<dimGrid,dimBlock>>>(d_buffer, size, d_histogram, nBins);
+	cudaDeviceSynchronize();
 
+	cudaMemcpy(h_histogram, d_histogram, nBins * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+
+	cudaDeviceSynchronize();
+
+	printf("Histogram:\n");
+	for (int i = 0; i < nBins; i++)
+	{
+		printf("Bin %d: %u\n", i, h_histogram[i]);
+	}
+	// Zwolnienie pamięci
+	free(h_buffer);
+	free(h_histogram);
+	cudaFree(d_buffer);
+	cudaFree(d_histogram);
 	///////////////////////////////////////////////////////
 
-	free(h_buffer);
+
 
 	return 0;
 }
